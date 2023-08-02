d11743ba9af8fea8919c14e84f957690b3de2d90
Daniel Silhavy <daniel.silhavy@fokus.fraunhofer.de>
2023-08-02 16:01:18 +0200

    Adds a Utils class with  functions that are useful for Media Stream Handler and Media Session Handler

---
+++ app/src/main/java/com/fivegmag/a5gmscommonlibrary/helpers/Utils.kt
@@ -0,0 +1,83 @@
+package com.fivegmag.a5gmscommonlibrary.helpers
+
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.util.Date
+import java.util.Random
+import java.util.TimeZone
+import okhttp3.Headers
+
+class Utils {
+
+    fun getCurrentTimestamp(): Long {
+        return System.currentTimeMillis();
+    }
+
+    fun convertTimestampToXsDateTime(timestampInMillis: Long): String {
+        // Create a Date object using the provided timestamp
+        val date = Date(timestampInMillis)
+
+        // Create a SimpleDateFormat object to format the date
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+        // Format the date to xs:datetime string
+        return dateFormat.format(date)
+    }
+
+    fun getCurrentXsDateTime(): String {
+        val currentTimestamp = getCurrentTimestamp()
+
+        return convertTimestampToXsDateTime(currentTimestamp)
+    }
+
+    fun millisecondsToISO8601(milliseconds: Long): String? {
+        // Create a Duration object from milliseconds
+        val duration: Duration = Duration.ofMillis(milliseconds)
+
+        // Extract the components from the duration
+        val years: Long = duration.toDays() / 365
+        val days: Long = duration.toDays() % 365
+        val hours: Long = duration.toHours() % 24
+        val minutes: Long = duration.toMinutes() % 60
+        val seconds: Long = duration.seconds % 60
+
+        // Construct the ISO 8601 period string
+        return "P" + years + "Y" + days + "D" +
+                "T" + hours + "H" + minutes + "M" + seconds + "S"
+    }
+
+    fun generateRandomFloat(): Float {
+        val random = Random()
+        return random.nextFloat() * 100.0f
+    }
+
+    fun addTrailingSlashIfNeeded(input: String): String {
+        return if (!input.endsWith("/")) {
+            "$input/"
+        } else {
+            input
+        }
+    }
+
+    fun hasResponseChanged(
+        headers: Headers,
+        previousResponseHeaders: Headers?
+    ): Boolean {
+        if (previousResponseHeaders == null) {
+            return true
+        }
+        val headersToValidate = arrayOf("last-modified", "etag")
+
+        return headersToValidate.all { header ->
+            hasHeaderChanged(
+                headers.get(header),
+                previousResponseHeaders.get(header)
+            )
+        }
+    }
+
+    private fun hasHeaderChanged(headerA: String?, headerB: String?): Boolean {
+        return headerA == null || headerB == null || headerA != headerB
+    }
+}
